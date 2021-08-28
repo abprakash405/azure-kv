8b949d4f52970bb26cc09f2990e2bd72f34f5134
abprakash405 <abprakash405@gmail.com>
2021-08-28 14:40:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy = true
-    }
-  }
-}
-
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "example" {
