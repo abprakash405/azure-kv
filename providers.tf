f6ffcd1509bae34078a0ba3b689cd2873fe694e6
abprakash405 <abprakash405@gmail.com>
2021-08-28 14:42:36 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -15,8 +15,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  var.subscription_id = var.ARM_SUBSCRIPTION_ID
-  var.client_id       = var.ARM_CLIENT_ID
-  var.client_secret   = var.ARM_CLIENT_SECRET
-  var.tenant_id       = var.ARM_TENANT_ID
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
 }
